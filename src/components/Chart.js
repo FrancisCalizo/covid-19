74b56fd97d0df59b010683c5ad26e4445488cca2
Francis Calizo <francisacalizo@gmail.com>
2020-03-30 17:40:34 -0400

    Work on labels

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,20 +1,38 @@
-import React, { useState } from "react"
+import React, { useState, useEffect, useContext } from "react"
 import { Line } from "react-chartjs-2"
 
+import CovidContext from "../context/covidContext"
+
 const Chart = () => {
-  const [data] = useState({
-    labels: ["March 1", "March 3", "March 5", "March 7", "March 8"],
+  const covidContext = useContext(CovidContext)
+  const { currentCountry, countryData } = covidContext
+
+  const [cases, setCases] = useState([])
+  const [dates, setDates] = useState([])
+
+  useEffect(() => {
+    // if (Object.keys(countryData).length) {
+    if (countryData[currentCountry] !== undefined) {
+      setDates(countryData[currentCountry].map(data => data.date))
+      console.log(countryData[currentCountry])
+    }
+  }, [])
+
+  const data = {
+    labels: dates,
     datasets: [
       {
         label: "Population",
-        data: [1000, 1200, 1649, 2123, 1789],
+        data: cases,
         // backgroundColor: "#4a5568",
         borderColor: "#f00",
         label: "This is a label",
         pointBackgroundColor: "green",
       },
     ],
-  })
+  }
+
+  console.log(countryData[currentCountry])
 
   return (
     <div>
