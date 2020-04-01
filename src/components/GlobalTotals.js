8be80634d6d08a0cfb6cd8eb91767fef4fab5414
Francis Calizo <francisacalizo@gmail.com>
2020-04-01 16:56:53 -0400

    Transition for page

--- src/components/GlobalTotals.js
+++ src/components/GlobalTotals.js
@@ -1,5 +1,6 @@
-import React, { useContext } from "react"
+import React, { useContext, useState, useEffect } from "react"
 import CountUp from "react-countup"
+import { Transition } from "react-transition-group"
 
 import CovidContext from "../context/covidContext"
 
@@ -7,66 +8,95 @@ const GlobalTotals = () => {
   const covidContext = useContext(CovidContext)
   const { globalTotals, date } = covidContext
 
+  const [isLoaded, setIsLoaded] = useState(false)
+
+  useEffect(() => {
+    console.log(isLoaded)
+    setIsLoaded(true)
+  }, [])
+
   return (
-    <div className="bg-gray-800 pt-6 sm:pt-2 pb-2">
-      <h1 className="text-white text-center text-2xl sm:text-3xl">
-        Global Statistics
-      </h1>
-      <div className="flex justify-center flex-wrap md:flex-no-wrap container mx-auto">
-        <div className="w-4/5 sm:w-2/5 md:w-1/5 mx-2">
-          <div className="border border-gray-600 rounded bg-gray-700 shadow-2xl my-2 py-3 px-4">
-            <h3 className="md:text-xs lg:text-sm xl:text-base inline-block font-bold text-gray-200 bg-blue-400 px-3 py-1 rounded-full">
-              Cases
-            </h3>
-            <h3 className="text-3xl md:text-xl lg:text-2xl xl:text-3xl text-right border-b-4 text-white border-blue-400 pb-4 py-2">
-              {!Object.keys(globalTotals).length ? (
-                "0"
-              ) : (
-                <CountUp separator="," end={globalTotals.cases} />
-              )}
-            </h3>
+    <Transition in={isLoaded} timeout={500}>
+      {state => (
+        <div className="bg-gray-800 pt-6 sm:pt-2 pb-2">
+          <h1 className="text-white text-center text-2xl sm:text-3xl">
+            Global Statistics
+          </h1>
+          <div
+            className={`${
+              isLoaded ? "opacity-100" : "opacity-0"
+            } totals-transition flex justify-center flex-wrap md:flex-no-wrap container mx-auto`}
+            state={state}
+          >
+            <div className="w-4/5 sm:w-2/5 md:w-1/5 mx-2">
+              <div className="border border-gray-600 rounded bg-gray-700 shadow-2xl my-2 py-3 px-4">
+                <h3 className="md:text-xs lg:text-sm xl:text-base inline-block font-bold text-gray-200 bg-blue-400 px-3 py-1 rounded-full">
+                  Cases
+                </h3>
+                <h3 className="text-3xl md:text-xl lg:text-2xl xl:text-3xl text-right border-b-4 text-white border-blue-400 pb-4 py-2">
+                  {!Object.keys(globalTotals).length ? (
+                    "0"
+                  ) : (
+                    <CountUp
+                      separator=","
+                      end={globalTotals.cases}
+                      delay={0.15}
+                    />
+                  )}
+                </h3>
+              </div>
+            </div>
+            <div className="w-4/5 sm:w-2/5 md:w-1/5 mx-2">
+              <div className="border border-gray-600 rounded bg-gray-700 shadow-2xl my-2 py-3 px-4">
+                <h3 className="md:text-xs lg:text-sm xl:text-base inline-block font-bold text-gray-200 bg-red-400 px-3 py-1 rounded-full">
+                  Deaths
+                </h3>
+                <h3 className="text-3xl md:text-xl lg:text-2xl xl:text-3xl text-right border-b-4 text-white border-red-400 pb-4 py-2">
+                  {!Object.keys(globalTotals).length ? (
+                    "0"
+                  ) : (
+                    <CountUp
+                      separator=","
+                      end={globalTotals.deaths}
+                      delay={0.15}
+                    />
+                  )}
+                </h3>
+              </div>
+            </div>
+            <div className="w-4/5 sm:w-2/5 md:w-1/5 mx-2">
+              <div className="border border-gray-600 rounded bg-gray-700 shadow-2xl my-2 py-3 px-4">
+                <h3 className="md:text-xs lg:text-sm xl:text-base inline-block font-bold text-gray-200 bg-green-400 px-3 py-1 rounded-full">
+                  Recovered
+                </h3>
+                <h3 className="text-3xl md:text-xl lg:text-2xl xl:text-3xl text-right border-b-4 text-white border-green-400 pb-4 py-2">
+                  {!Object.keys(globalTotals).length ? (
+                    "0"
+                  ) : (
+                    <CountUp
+                      separator=","
+                      end={globalTotals.recovered}
+                      delay={0.15}
+                    />
+                  )}
+                </h3>
+              </div>
+            </div>
+            <div className="w-4/5 sm:w-2/5 md:w-1/5 mx-2">
+              <div className="border border-gray-600 rounded bg-gray-700 shadow-2xl my-2 py-3 px-4">
+                <h3 className="md:text-xs lg:text-sm xl:text-base inline-block font-bold text-gray-700 bg-yellow-400 px-3 py-1 rounded-full">
+                  Last Update
+                </h3>
+                <h3 className="text-3xl md:text-xl lg:text-2xl xl:text-3xl text-right border-b-4 text-white border-yellow-400 pb-4 py-2">
+                  {`${date.getMonth() +
+                    1}/${date.getDate()}/${date.getFullYear()}`}
+                </h3>
+              </div>
+            </div>
           </div>
         </div>
-        <div className="w-4/5 sm:w-2/5 md:w-1/5 mx-2">
-          <div className="border border-gray-600 rounded bg-gray-700 shadow-2xl my-2 py-3 px-4">
-            <h3 className="md:text-xs lg:text-sm xl:text-base inline-block font-bold text-gray-200 bg-red-400 px-3 py-1 rounded-full">
-              Deaths
-            </h3>
-            <h3 className="text-3xl md:text-xl lg:text-2xl xl:text-3xl text-right border-b-4 text-white border-red-400 pb-4 py-2">
-              {!Object.keys(globalTotals).length ? (
-                "0"
-              ) : (
-                <CountUp separator="," end={globalTotals.deaths} />
-              )}
-            </h3>
-          </div>
-        </div>
-        <div className="w-4/5 sm:w-2/5 md:w-1/5 mx-2">
-          <div className="border border-gray-600 rounded bg-gray-700 shadow-2xl my-2 py-3 px-4">
-            <h3 className="md:text-xs lg:text-sm xl:text-base inline-block font-bold text-gray-200 bg-green-400 px-3 py-1 rounded-full">
-              Recovered
-            </h3>
-            <h3 className="text-3xl md:text-xl lg:text-2xl xl:text-3xl text-right border-b-4 text-white border-green-400 pb-4 py-2">
-              {!Object.keys(globalTotals).length ? (
-                "0"
-              ) : (
-                <CountUp separator="," end={globalTotals.recovered} />
-              )}
-            </h3>
-          </div>
-        </div>
-        <div className="w-4/5 sm:w-2/5 md:w-1/5 mx-2">
-          <div className="border border-gray-600 rounded bg-gray-700 shadow-2xl my-2 py-3 px-4">
-            <h3 className="md:text-xs lg:text-sm xl:text-base inline-block font-bold text-gray-700 bg-yellow-400 px-3 py-1 rounded-full">
-              Last Update
-            </h3>
-            <h3 className="text-3xl md:text-xl lg:text-2xl xl:text-3xl text-right border-b-4 text-white border-yellow-400 pb-4 py-2">
-              {`${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`}
-            </h3>
-          </div>
-        </div>
-      </div>
-    </div>
+      )}
+    </Transition>
   )
 }
 
--- src/components/layouts/Global.js
+++ src/components/layouts/Global.js
@@ -1,4 +1,5 @@
-import React, { useContext } from "react"
+import React, { useContext, useState, useEffect } from "react"
+import { Transition } from "react-transition-group"
 
 import CovidContext from "../../context/covidContext"
 import LineChart from "../LineChart"
@@ -6,35 +7,49 @@ import BarChart from "../BarChart"
 
 const Global = () => {
   const covidContext = useContext(CovidContext)
-  const { countries, currentCountry, setCurrentCountry } = useContext(
-    CovidContext
-  )
+
+  const { countries, setCurrentCountry } = covidContext
+
+  const [isLoaded, setIsLoaded] = useState(false)
+
+  useEffect(() => {
+    console.log(isLoaded)
+    setIsLoaded(true)
+  }, [])
 
   const handleChange = e => {
     setCurrentCountry(e.target.value)
   }
 
   return (
-    <div>
-      <div className="container my-8 mx-auto px-8 bg-gray-800">
-        <h1 className="text-3xl text-center text-white">
-          Statistics By Country
-        </h1>
-        <select
-          className="h-12 mb-4 mt-3 text-center text-xl block mx-auto w-64 text-gray-300 bg-gray-700 border border-gray-800 hover:border-blue-700 rounded
+    <Transition in={isLoaded} timeout={500}>
+      {state => (
+        <div>
+          <div
+            className={`${
+              isLoaded ? "opacity-100" : "opacity-0"
+            } totals-transition container my-8 mx-auto px-8 bg-gray-800`}
+          >
+            <h1 className="text-3xl text-center text-white">
+              Statistics By Country
+            </h1>
+            <select
+              className="h-12 mb-4 mt-3 text-center text-xl block mx-auto w-64 text-gray-300 bg-gray-700 border border-gray-800 hover:border-blue-700 rounded
         shadow leading-tight focus:outline-none shadow-outline cursor-pointer"
-          onChange={handleChange}
-        >
-          {countries.map(country => (
-            <option key={country} value={country}>
-              {country}
-            </option>
-          ))}
-        </select>
-        <BarChart />
-        <LineChart />
-      </div>
-    </div>
+              onChange={handleChange}
+            >
+              {countries.map(country => (
+                <option key={country} value={country}>
+                  {country}
+                </option>
+              ))}
+            </select>
+            <BarChart />
+            <LineChart />
+          </div>
+        </div>
+      )}
+    </Transition>
   )
 }
 
