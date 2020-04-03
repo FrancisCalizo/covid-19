3fd257d86b0fe81413415f6e8848c1023293ec60
Francis Calizo <francisacalizo@gmail.com>
2020-04-03 16:37:13 -0400

    Global to USA mvp working

--- src/components/layouts/Global.js
+++ src/components/layouts/Global.js
@@ -8,7 +8,13 @@ import BarChart from "../BarChart"
 const Global = () => {
   const covidContext = useContext(CovidContext)
 
-  const { countries, setCurrentCountry, isUsOnly } = covidContext
+  const {
+    countries,
+    setCurrentCountry,
+    isUsOnly,
+    states,
+    setCurrentState,
+  } = covidContext
 
   const [isLoaded, setIsLoaded] = useState(false)
 
@@ -16,8 +22,18 @@ const Global = () => {
     setIsLoaded(true)
   }, [])
 
+  useEffect(() => {
+    if (isUsOnly) {
+      setCurrentState("Alabama")
+    } else {
+      setCurrentCountry("Afghanistan")
+    }
+  }, [isUsOnly])
+
   const handleChange = e => {
-    setCurrentCountry(e.target.value)
+    isUsOnly
+      ? setCurrentState(e.target.value)
+      : setCurrentCountry(e.target.value)
   }
 
   return (
@@ -33,18 +49,25 @@ const Global = () => {
               Statistics By {isUsOnly ? "State" : "Country"}
             </h1>
             <select
-              className="h-12 mb-4 mt-3 text-center text-xl block mx-auto w-64 text-gray-300 bg-gray-700 border border-gray-800 hover:border-blue-700 rounded
-        shadow leading-tight focus:outline-none shadow-outline cursor-pointer"
+              className="h-12 mb-4 mt-3 text-center text-xl block mx-auto w-64 text-gray-300 bg-gray-700
+              border border-gray-800 hover:border-blue-700 rounded
+              shadow leading-tight focus:outline-none shadow-outline cursor-pointer"
               onChange={handleChange}
             >
-              {countries.map(country => (
-                <option key={country} value={country}>
-                  {country}
-                </option>
-              ))}
+              {isUsOnly
+                ? states.map(state => (
+                    <option key={state} value={state}>
+                      {state}
+                    </option>
+                  ))
+                : countries.map(country => (
+                    <option key={country} value={country}>
+                      {country}
+                    </option>
+                  ))}
             </select>
             <BarChart />
-            <LineChart />
+            {!isUsOnly && <LineChart />}
           </div>
         </div>
       )}
